49598827d225137c4e270282171681a184c89a0d
Agedtech <72783369+agedtech@users.noreply.github.com>
2021-12-29 04:16:26 -0800

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-
+from updater import views
 from .views import (
     dashboard,
     profile,
@@ -32,4 +32,5 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('update_server/', views.update, name = 'update'),
 ]
